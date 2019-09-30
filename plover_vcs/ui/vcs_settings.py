baa9000fba469646379b2165d8e6ec756cfb0a7a
Levi Miller <levi.miller@qhrtech.com>
2019-09-29 22:52:47 -0700

    Add title and icon

--- plover_vcs/ui/vcs_settings.py
+++ plover_vcs/ui/vcs_settings.py
@@ -1,15 +1,18 @@
-from PyQt5.QtWidgets import QDialog, QFileDialog
+from PyQt5.QtWidgets import QFileDialog
 
 from plover.gui_qt.tool import Tool
 from plover_vcs.ui.vcs_settings_ui import Ui_VcsSettings
 from plover_vcs.vcs_config import VcsConfig, CONFIG_MANAGER
 
 
-class VcsSettings(Tool, QDialog, Ui_VcsSettings):
+class VcsSettings(Tool, Ui_VcsSettings):
+    TITLE = 'Vcs Settings'
+    ICON = ':/plover_vcs/icon.svg'
 
-    def __init__(self):
-        super(VcsSettings, self).__init__()
+    def __init__(self, engine):
+        super(VcsSettings, self).__init__(engine)
         self.setupUi(self)
+        self.__engine = engine
 
     def add_dictionary(self):
         self.dictionariesListWidget.addItem('<dictionary>')
